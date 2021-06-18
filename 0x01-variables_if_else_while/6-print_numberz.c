f26cd8cdb53d4771cef9d99b0afe57604c44f9fa
Nonny Jonas <edecjonas@gmail.com>
2021-06-18 12:31:06 -0700

    Added files: 6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program will print all single digit
+ * numbers of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
