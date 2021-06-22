4b7c8be87250249a003fe6b8409808505f04d635
Nonny Jonas <edecjonas@gmail.com>
2021-06-22 12:18:15 -0700

    Added files: 7-print_last_digit.c, 7-main.c, updated: holberton.h

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int negate = -1;
+	int a;
+
+	if (n < 0)
+		n = n * negate;
+
+	a = n % 10;
+
+	_putchar(a + '0');
+
+	return (a);
+}
