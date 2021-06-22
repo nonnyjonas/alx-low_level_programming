0ba3b1af98f6cc6364669780caf89e3ddb78c5bc
Nonny Jonas <edecjonas@gmail.com>
2021-06-22 10:41:43 -0700

    Added files: 4-isalpha.c, 4-main.c, updated: holberton.h

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks for alphabetic character
+ * @c: the character to check
+ * Return: 1 if c is alphabet, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
