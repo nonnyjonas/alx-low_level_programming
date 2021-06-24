e107eb12a876bf41015c713a72bb14134847bc26
Nonny Jonas <edecjonas@gmail.com>
2021-06-24 12:32:07 -0700

    Added Task 5 files

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - 10 times the numbers, from 0 to 14,
+ * followed by a new line.
+ */
+void more_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		int j = 0;
+
+		while (j < 15)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+
+			j++;
+		}
+
+		_putchar('\n');
+		i++;
+
+	}
+}
