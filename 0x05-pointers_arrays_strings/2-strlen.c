251d926a83c260dc6b05d77c8a9126678d055465
Nonny Jonas <edecjonas@gmail.com>
2021-06-28 12:04:21 -0700

    Added Task 2 files

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to return length
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
