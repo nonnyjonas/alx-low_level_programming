e8afb90e53fe83630abce5ad0e239a08c2909192
Nonny Jonas <edecjonas@gmail.com>
2021-07-19 15:42:02 -0700

    Added Description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef FILE_DOG_H
 #define FILE_DOG_H
 
+/**
+ * struct dog - new type struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: new type struct dog with the above elements
+ */
 struct dog
 {
 	char *name;
